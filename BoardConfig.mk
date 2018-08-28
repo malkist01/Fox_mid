7d69b9b37f19355c5e4f91dbfc8b600c9597b88e
Darth9 <reyprimerjefe@gmail.com>
2018-08-28 10:48:23 +0100

    Updated fstab to fix LOS-15.x encryption isses (last 16kb needed by LOS for encryption stuff)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,8 @@ TW_THEME := portrait_hdpi
 TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_NTFS_3G := true
 TW_MAX_BRIGHTNESS := 255
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun0/file"
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun0/file"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_SUPERSU := true
 TW_SCREEN_BLANK_ON_BOOT := true
@@ -111,5 +112,4 @@ BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
 TARGET_COPY_OUT_VENDOR := vendor
 endif
 # DJ9
-
 
