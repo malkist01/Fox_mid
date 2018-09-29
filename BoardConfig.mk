8a0dbb6bcfced8dc26ca66cad12bb4745a9d589a
MrYacha <YachaGorobtsov@gmail.com>
2018-09-29 17:36:10 +0300

    Decrypt FBE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
